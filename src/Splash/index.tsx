e6147993566d9abfee8b2f460122df541cfb4809
charlespereira1 <charles.spereira-ext@viavarejo.com.br>
2021-02-15 14:57:01 -0300

    restruct routes default complete

--- src/Splash/index.tsx
+++ src/Splash/index.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import LottieView from "lottie-react-native";
 
 import rocket from "../assets/rocket.json";
 
-const Splash: React.FC = () => (
-  <SafeAreaView
-    style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <LottieView
-      resizeMode="contain"
-      autoSize
-      source={rocket}
-      autoPlay
-      loop={false}
-      speed={0.5}
-      onAnimationFinish={() => {
-        console.log("Animated Finished!");
+const Splash: React.FC = () => {
+  const navigate = useNavigation();
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
       }}
-    />
-  </SafeAreaView>
-);
+    >
+      <LottieView
+        resizeMode="contain"
+        autoSize
+        source={rocket}
+        autoPlay
+        loop={false}
+        speed={0.5}
+        onAnimationFinish={() => {
+          console.log("Animated Finished!");
+          navigate.navigate("Home");
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 export default Splash;
