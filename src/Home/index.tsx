7839c49f39d6374ba495c7ac38f7bc47a36e261d
charlespereira1 <charles.spereira-ext@viavarejo.com.br>
2021-02-15 15:24:00 -0300

    Home styles

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 
-import { Text } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const Home: React.FC = () => {
   return (
-    <>
-      <Text>Home</Text>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#f00",
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+});
 
 export default Home;
